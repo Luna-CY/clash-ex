5933230b8ae4a5666c245a737227a680f34bc4c2
萤火 <luna@cyl-mail.com>
2022-09-29 22:32:37 +0800

    init

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../static/css/App.css';
-import {Button, Divider} from "@douyinfe/semi-ui";
-import {IconGithubLogo} from "@douyinfe/semi-icons";
+import {TabPane, Tabs} from "@douyinfe/semi-ui";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-Header">
-          <div className="Logo">Clash Ex</div>
-          <IconGithubLogo/>
+      <div className="App-Header"></div>
+      <div className="App-Content">
+        <Tabs className="Tabs" lazyRender={true}>
+          <TabPane itemKey="home" tab="首页">123</TabPane>
+          <TabPane itemKey="rule" tab="规则">111</TabPane>
+          <TabPane itemKey="proxy" tab="代理">123</TabPane>
+          <TabPane itemKey="connection" tab="连接">111</TabPane>
+          <TabPane itemKey="setting" tab="设置">222</TabPane>
+        </Tabs>
       </div>
-      <Divider ></Divider>
     </div>
   );
 }
